0f3f5ecb8b9ef187d4e294d435a3ddd7abff5793
Ryan Ciehanski <ryan@ciehanski.com>
2018-09-09 20:02:15 -0500

    Add trip creation functionality

--- __ayetravel__/app/models.py
+++ __ayetravel__/app/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 
 class Trips(models.Model):
-    id = models.IntegerField(primary_key=True)
-    location = models.CharField(max_length=50)
+    #id = models.IntegerField(primary_key=True)
+    #location = models.CharField(max_length=50)
+    pass
 
 
 class Notifications(models.Model):
-    text = models.CharField(max_length=200)
+    #text = models.CharField(max_length=200)
+    pass
 
 
 class UserSettings(models.Model):
-    profile_picture = models.ImageField()
+    #profile_picture = models.ImageField()
+    pass
