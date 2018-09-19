135b26fb23f0f2278354d1928a833e1b6fbf0304
Ryan Ciehanski <ryan@ciehanski.com>
2018-09-19 18:45:51 -0500

    Fix CreateTripForm model form

--- __ayetravel__/trips/views.py
+++ __ayetravel__/trips/views.py
@@ -1,30 +1,20 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, UpdateView, DeleteView, ListView, FormView
 from trips.forms import CreateTripForm
 from trips.models import Trips
-from app.views import render_user_trips, render_community_trips, render_user_notifications
 from app.views import IndexView
 
 
-class TripsDetailed(LoginRequiredMixin, DetailView):
+class TripsDetailed(IndexView, DetailView):
     context_object_name = 'trips_detailed'
     template_name = 'trips/trips_detailed.html'
     object = Trips
 
     def get(self, request, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['community_trips'] = render_community_trips(request)
         context['trips'] = self.clean_object(request)
-        context['notifs'] = render_user_notifications(request)
-        context['trips_total'] = len(render_user_trips(request))
-        context['notifs_total'] = len(render_user_notifications(request))
         return render(request, self.template_name, context)
-
-    def get_object(self, queryset=Trips):
-        slug_ = self.kwargs.get('slug')
-        return get_object_or_404(Trips, slug=slug_)
 
     def clean_object(self, request):
         slug_ = self.kwargs.get('slug')
@@ -46,51 +36,27 @@ class CreateTrip(IndexView, FormView):
         return super().form_valid(form)
 
 
-class UpdateTrip(LoginRequiredMixin, UpdateView):
+class UpdateTrip(IndexView, UpdateView):
     context_object_name = 'update_trip'
     template_name = 'trips/create_trip.html'
     # form_class = UpdateTripForm
-
-    def get(self, request, *args, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['trips'] = self.get_object()
-        context['notifs'] = render_user_notifications(request)
-        context['notifs_total'] = len(render_user_notifications(request))
-        return render(request, self.template_name, context)
-
-    def get_object(self, queryset=Trips):
-        slug_ = self.kwargs.get('slug')
-        return get_object_or_404(Trips, slug=slug_)
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
 
 
-class DeleteTrip(LoginRequiredMixin, DeleteView):
+class DeleteTrip(IndexView, DeleteView):
     context_object_name = 'update_trip'
     template_name = 'trips/create_trip.html'
     model = Trips
     success_url = reverse_lazy('app:trips_list')
 
-    def get_object(self, queryset=Trips):
-        slug_ = self.kwargs.get('slug')
-        return get_object_or_404(Trips, slug=slug_)
-
     def post(self, request, *args, **kwargs):
         pass
 
 
-class TripsList(LoginRequiredMixin, ListView):
+class TripsList(IndexView, ListView):
     context_object_name = 'trips_list'
     template_name = 'trips/trips_list.html'
     object_list = Trips.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        self.object_list = render_user_trips(request)
-        context = super().get_context_data(**kwargs)
-        context['trips'] = render_user_trips(request)
-        context['notifs'] = render_user_notifications(request)
-        context['trips_total'] = len(render_user_trips(request))
-        context['notifs_total'] = len(render_user_notifications(request))
-        return render(request, self.template_name, context)
